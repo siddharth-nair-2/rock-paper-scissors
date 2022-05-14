08518171d8da0c0f4d5df104f63034b88cf0198f
Siddharth Nair <nairsid2@gmail.com>
2022-05-14 12:38:50 +0530

    created the computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+let playerScore = 0, computerScore = 0;
+let playerSelection;
+let computerSelection;
+
+function computerPlay() {
+    var options = ["Rock", "Paper", "Scissors"];
+    var random = options[Math.floor(Math.random() * options.length)];
+
+    return random;
+}
