00fab00c08664cde8a43ae6b92e9cb03b2e7bc35
Siddharth Nair <nairsid2@gmail.com>
2022-05-14 12:41:25 +0530

    Added functionality for score in the single round

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-function computerPlay(){
+let playerScore = 0, computerScore = 0;
+
+function computerPlay() {
     var options = ["Rock", "Paper", "Scissors"];
     var random = options[Math.floor(Math.random() * options.length)];
 
     return random;
 }
 
-function singleRound(playerSelection, computerSelection){
+function singleRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    if(playerSelection === "rock"){
-        if(computerSelection === "rock") return "It is a draw! Both chose Rock!";
-        else if(computerSelection === "paper") return "You Lose! Paper beats Rock!";
-        else return "You Win! Rock beats Scissors!"
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            return "It is a draw! Both chose Rock!";
+        }
+        else if (computerSelection === "paper") {
+            computerScore++;
+            return "You Lose! Paper beats Rock!";
+        }
+        else { 
+            playerScore++;
+            return "You Win! Rock beats Scissors!" 
+        }
     }
 
-    else if(playerSelection === "paper"){
-        if(computerSelection === "paper") return "It is a draw! Both chose Paper!";
-        else if(computerSelection === "rock") return "You Win! Paper beats Rock!";
-        else return "You Lose! Scissors beats Paper!"
+    else if (playerSelection === "paper") {
+        if (computerSelection === "paper") return "It is a draw! Both chose Paper!";
+        else if (computerSelection === "rock"){ 
+            playerScore++; 
+            return "You Win! Paper beats Rock!";
+        }
+        else{ 
+            computerScore++;
+            return "You Lose! Scissors beats Paper!";
+        }
+
     }
 
-    else if(playerSelection === "scissors"){
-        if(computerSelection === "scissors") return "It is a draw! Both chose Scissors!";
-        else if(computerSelection === "rock") return "You Lose! Rock beats Scissors!";
-        else return "You Win! Scissors beats Paper!"
+    else if (playerSelection === "scissors") {
+        if (computerSelection === "scissors") return "It is a draw! Both chose Scissors!";
+        else if (computerSelection === "rock"){
+            computerScore++;
+            return "You Lose! Rock beats Scissors!";
+        }
+        else{
+            playerScore++;
+            return "You Win! Scissors beats Paper!";
+        }
     }
-    else{
+    else {
         return "Sorry! You had an invalid choice!"
     }
 }
@@ -35,3 +58,7 @@ const playerSelection = "paper";
 const computerSelection = computerPlay();
 
 console.log(singleRound(playerSelection, computerSelection));
+
+function game() {
+    
+}
